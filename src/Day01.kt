e4b69d0c647d758dcfb7aa95ce5d210c7fa1b5a5
nohum <nohum@users.noreply.github.com>
2021-12-01 13:15:20 +0100

    feat: day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,19 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
-    }
-
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
-
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
     val input = readInput("Day01")
-    println(part1(input))
-    println(part2(input))
+
+    val simpleIncreases = input.map { value -> Integer.parseInt(value) }
+        .zipWithNext()
+        .map { (first, second) -> second > first }
+        .count { result -> result };
+
+    println("Simple increases (part one): $simpleIncreases")
+
+    val windowedIncreases = input.map { value -> Integer.parseInt(value) }
+        .windowed(3)
+        .map { (first, second, third) -> first + second + third }
+        .zipWithNext()
+        .map { (first, second) -> second > first }
+        .count { result -> result };
+
+    println("Windowed increases (part two): $windowedIncreases")
 }
